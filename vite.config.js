c6e8c12ebf12d3aa35ee200837c8980312b74fb3
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-12-09 16:16:46 +0200

    fixed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import VitePluginVueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/vue-todo-app/",
+  base: "/",
   plugins: [vue(), VitePluginVueDevTools()],
   resolve: {
     alias: {
