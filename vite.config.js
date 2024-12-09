ce57665023218d1409e68f1056219fc0894ab8e6
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-12-09 16:11:38 +0200

    fixed

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import VitePluginVueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/vue-todo-app/",
-  plugins: [vue()],
+  plugins: [vue(), VitePluginVueDevTools()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
